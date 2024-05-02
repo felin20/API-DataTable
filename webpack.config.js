e4cc748ebfa93cbd9b58cacc010de0a6aca743c8
felin20 <felingenjosemon@gmail.com>
2024-05-02 12:55:27 +0530

    Your commit message here

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
