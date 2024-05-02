82a1c2af04f96cfda6b981b3d624aadee75313ac
felin20 <felingenjosemon@gmail.com>
2024-05-02 11:48:12 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import enTranslations from '@shopify/polaris/locales/en.json';
-import { Switch } from 'react-router-dom';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ProductListingPage from './components/ProductListingPage';
-import ProductModal from './components/ProductModal'; // Make sure the path is correct
 import { AppProvider } from '@shopify/polaris';
+import ProductListingPage from './components/ProductListingPage';
+import ProductModal from './components/ProductModal';
 
 function App() {
   return (
     <AppProvider i18n={enTranslations}>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ProductListingPage} />
-        {/* Add more routes if necessary */}
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ProductListingPage} />
+          {/* Add more routes if necessary */}
+        </Switch>
+      </Router>
     </AppProvider>
   );
 }
 
 export default App;
+
+
+// import React from 'react';
+// import enTranslations from '@shopify/polaris/locales/en.json';
+// import { Switch } from 'react-router-dom';
+
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import ProductListingPage from './components/ProductListingPage';
+// import ProductModal from './components/ProductModal'; // Make sure the path is correct
+// import { AppProvider } from '@shopify/polaris';
+
+// function App() {
+//   return (
+//     <AppProvider i18n={enTranslations}>
+//     <Router>
+//       <Switch>
+//         <Route exact path="/" component={ProductListingPage} />
+//         {/* Add more routes if necessary */}
+//       </Switch>
+//     </Router>
+//     </AppProvider>
+//   );
+// }
+
+// export default App;
 
 
 // import React from 'react';
